a2099e5f309e42ce16f99a76174681c254efb2e9
Jericon86 <valdead1@gmail.com>
2020-11-19 19:41:54 +0500

    Ver2

--- HW№2.py
+++ HW№2.py
@@ -1,7 +1,7 @@
-account1 = {'login':'ivan', 'password': 'q1'}
-account2 = {'login':'petr', 'password': 'q2'}
-account3 = {'login':'olga', 'password': 'q3'}
-account4 = {'login':'anna', 'password': 'q4'}
+account1 = {'login': 'ivan', 'password': 'q1'}
+account2 = {'login': 'petr', 'password': 'q2'}
+account3 = {'login': 'olga', 'password': 'q3'}
+account4 = {'login': 'anna', 'password': 'q4'}
 
 user1 = {'name': 'Иван', 'age': 20, 'account': account1}
 user2 = {'name': 'Петр', 'age': 25, 'account': account2}
@@ -11,7 +11,7 @@ user4 = {'name': 'Анна', 'age': 27, 'account': account4}
 users = [user1, user2, user3, user4]
 accounts = [account1, account2, account3, account4]
 
-task_1=input("Введите ключ (name или account):").lower()
+task_1 = input("Введите ключ (name или account):").lower()
 
 try:
     print(f"значение ключа {task_1} для юзера 1 = {user1[task_1]}")
@@ -24,14 +24,15 @@ except:
 user_key = int(input('Введите порядковый номер:'))
 try:
     print(f"Данные по юзеру № {user_key}: \n"
-        f"Имя: {users[user_key-1]['name']} \n"
-        f"Возраст: {users[user_key-1]['age']} \n"
-        f"Аккаунт: {accounts[user_key-1]['login']} \n"
-        f"Пароль: {accounts[user_key-1]['password']}")
+          f"Имя: {users[user_key-1]['name']} \n"
+          f"Возраст: {users[user_key-1]['age']} \n"
+          f"Аккаунт: {accounts[user_key-1]['login']} \n"
+          f"Пароль: {accounts[user_key-1]['password']}")
 except:
     print('Пользователь с указанным номером не найден')
 
-task_3 = int(input('Введите номер пользователя которого хотите переместить в конец:'))
+task_3 = int(
+    input('Введите номер пользователя которого хотите переместить в конец:'))
 print('Список до изменеия:')
 print(users)
 print(f"Ползователь с именем{users[task_3-1]['name']} перемещен в конец")
@@ -39,3 +40,6 @@ element = users.pop(task_3-1)
 users.append(element)
 print('Список после изменеия:')
 print(users)
+lenght = len(users)
+midAge = (user1['age']+user2['age']+user3['age']+user4['age'])/lenght
+print(f"Средний возраст пользователей: {midAge}")
